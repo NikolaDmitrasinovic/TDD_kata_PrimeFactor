33258136ef385ada6c63cf95f6f282fac58091d6
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-11-30 15:29:29 +0100

    refactor: improve code in Generate method refactor: delete comments from tests

--- PrimeFactors/PrimeFactors.cs
+++ PrimeFactors/PrimeFactors.cs
@@ -4,35 +4,13 @@
     {
         public static List<int> Generate(int number)
         {
-            List<int> factors = [];
+            List<int> primes = [];
 
-            int candidate = 2;
+            for (int candidate = 2; number > 1; candidate++)
+                for (; number%candidate == 0; number /= candidate)
+                    primes.Add(candidate);
 
-            while (number > 1)
-            {
-                while (number % candidate == 0)
-                {
-                    factors.Add(candidate);
-                    number /= candidate;
-                }
-
-                candidate++;
-            }
-
-            return factors;
+            return primes;
         }
-
-        // first try code ( followed the guide)
-        //public static List<int> Generate(int number)
-        //{
-        //    List<int> primes = [];
-
-        //    for (int candidate = 2; number > 1; candidate++)
-        //        for (; number % candidate == 0; number /= candidate)
-        //            primes.Add(candidate);
-
-
-        //    return primes;
-        //}
     }
 }
